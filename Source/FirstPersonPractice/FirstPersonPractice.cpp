305ec2dccfd021fe3e5f66a118929f2de072f291
Ella Zhang <ellazhang37@gmail.com>
2022-12-07 01:05:46 -0400

    initial commit

---
+++ Source/FirstPersonPractice/FirstPersonPractice.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstPersonPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstPersonPractice, "FirstPersonPractice" );
+ 
